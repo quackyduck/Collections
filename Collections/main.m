4adf6ec97cf15147e161c4e1b72c1d684cd9c0a3
Nicolas Melo <nicolas@path.com>
2014-08-08 20:40:27 -0700

    Initial Commit

---
+++ Collections/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Collections
+//
+//  Created by Nicolas Melo on 8/8/14.
+//  Copyright (c) 2014 melo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
